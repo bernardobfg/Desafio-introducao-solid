4db4cf5ca85ee5e9b8d26d62949f909d28f371d8
bernardobfg <64651224+bernardobfg@users.noreply.github.com>
2021-07-07 11:53:40 -0300

    desafio feito

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,11 +3,14 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+        console.log(typeof user_id);
+        const user = this.showUserProfileUseCase.execute({ user_id });
+        return response.json(user);
+    }
 }
 
 export { ShowUserProfileController };
